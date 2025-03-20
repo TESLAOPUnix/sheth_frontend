67b358d04a6f8702a7a3f346f893874c70b8132c
Sahil Kumar <sk2003311@gmail.com>
2025-03-20 18:11:05 +0530

    conditions removed from options in 3m heatshrink

--- src/app/(products)/3m/heat-shrink-cable-jointing-kit/page.tsx
+++ src/app/(products)/3m/heat-shrink-cable-jointing-kit/page.tsx
@@ -51,9 +51,9 @@ export default function Component() {
 
   const productImages = [
     "/3m/HEAT_SHRINK_JOINTING_KIT/img1.png",
-  "/3m/HEAT_SHRINK_JOINTING_KIT/img2.png",
-  "/3m/HEAT_SHRINK_JOINTING_KIT/img3.png",
-  "/3m/HEAT_SHRINK_JOINTING_KIT/img4.jpg",
+    "/3m/HEAT_SHRINK_JOINTING_KIT/img2.png",
+    "/3m/HEAT_SHRINK_JOINTING_KIT/img3.png",
+    "/3m/HEAT_SHRINK_JOINTING_KIT/img4.jpg",
   ];
 
   const allVoltageOptions = [
@@ -86,62 +86,62 @@ export default function Component() {
     "800mm",
     "1000mm",
   ];
-  const cableTypeOptions = ["XLPE/PVC", "EPR", "Aerial Bunched Cable (ABC)"];
+  const cableTypeOptions = ["XLPE", "PVC", "EPR", "Aerial Bunched Cable (ABC)"];
 
-  useEffect(() => {
-    updateAvailableOptions();
-  }, [formData.cableType, formData.voltage]);
+  // useEffect(() => {
+  //   updateAvailableOptions();
+  // }, [formData.cableType, formData.voltage]);
 
-  const updateAvailableOptions = () => {
-    let voltages: string[] = [];
-    let cores: string[] = [];
-    let terminations: string[] = [];
+  // const updateAvailableOptions = () => {
+  //   let voltages: string[] = [];
+  //   let cores: string[] = [];
+  //   let terminations: string[] = [];
 
-    if (formData.cableType === "XLPE/PVC") {
-      voltages = allVoltageOptions.filter(
-        (v) => v === "1.1KV(E)" || !v.includes("UE")
-      );
-      terminations = ["indoor", "outdoor", "straight-through"];
-      if (formData.voltage === "1.1KV(E)") {
-        cores = allCoreOptions;
-      } else {
-        cores = ["1 core", "3 core"];
-      }
-    } else if (formData.cableType === "Aerial Bunched Cable (ABC)") {
-      voltages = [
-        "6.6KV(UE)/11KV(E)",
-        "11KV(UE)/22KV(E)",
-        "22KV(E)",
-        "33KV(E)",
-        "33KV(UE)",
-      ];
-      cores = ["1 core"];
-      terminations = ["outdoor", "straight-through"];
-    } else if (formData.cableType === "EPR") {
-      voltages = allVoltageOptions;
-      cores = allCoreOptions;
-      terminations = ["indoor", "outdoor", "straight-through"];
-    } else {
-      voltages = allVoltageOptions;
-      cores = allCoreOptions;
-      terminations = ["indoor", "outdoor", "straight-through"];
-    }
+  //   if (formData.cableType === "XLPE/PVC") {
+  //     voltages = allVoltageOptions.filter(
+  //       (v) => v === "1.1KV(E)" || !v.includes("UE")
+  //     );
+  //     terminations = ["indoor", "outdoor", "straight-through"];
+  //     if (formData.voltage === "1.1KV(E)") {
+  //       cores = allCoreOptions;
+  //     } else {
+  //       cores = ["1 core", "3 core"];
+  //     }
+  //   } else if (formData.cableType === "Aerial Bunched Cable (ABC)") {
+  //     voltages = [
+  //       "6.6KV(UE)/11KV(E)",
+  //       "11KV(UE)/22KV(E)",
+  //       "22KV(E)",
+  //       "33KV(E)",
+  //       "33KV(UE)",
+  //     ];
+  //     cores = ["1 core"];
+  //     terminations = ["outdoor", "straight-through"];
+  //   } else if (formData.cableType === "EPR") {
+  //     voltages = allVoltageOptions;
+  //     cores = allCoreOptions;
+  //     terminations = ["indoor", "outdoor", "straight-through"];
+  //   } else {
+  //     voltages = allVoltageOptions;
+  //     cores = allCoreOptions;
+  //     terminations = ["indoor", "outdoor", "straight-through"];
+  //   }
 
-    setAvailableVoltages(voltages);
-    setAvailableCores(cores);
-    setAvailableTerminations(terminations);
+  //   setAvailableVoltages(voltages);
+  //   setAvailableCores(cores);
+  //   setAvailableTerminations(terminations);
 
-    // Reset voltage, core, and termination if they're not in the new available options
-    if (!voltages.includes(formData.voltage)) {
-      setFormData((prev) => ({ ...prev, voltage: "" }));
-    }
-    if (!cores.includes(formData.core)) {
-      setFormData((prev) => ({ ...prev, core: "" }));
-    }
-    if (!terminations.includes(formData.termination)) {
-      setFormData((prev) => ({ ...prev, termination: "" }));
-    }
-  };
+  //   // Reset voltage, core, and termination if they're not in the new available options
+  //   if (!voltages.includes(formData.voltage)) {
+  //     setFormData((prev) => ({ ...prev, voltage: "" }));
+  //   }
+  //   if (!cores.includes(formData.core)) {
+  //     setFormData((prev) => ({ ...prev, core: "" }));
+  //   }
+  //   if (!terminations.includes(formData.termination)) {
+  //     setFormData((prev) => ({ ...prev, termination: "" }));
+  //   }
+  // };
 
   const handleInputChange = (field: string, value: string | number) => {
     setFormData((prev) => ({ ...prev, [field]: value }));
@@ -177,7 +177,13 @@ export default function Component() {
     updatingCart(sku, formData.quantity, name, terminationType, formData);
   };
 
-  const updatingCart = async (sku: string, quantity: any, name: string, terminationType: string, formData: any) => {
+  const updatingCart = async (
+    sku: string,
+    quantity: any,
+    name: string,
+    terminationType: string,
+    formData: any
+  ) => {
     try {
       const res = await axios.post(
         `${process.env.NEXT_PUBLIC_BACKEND_URL}/order`,
@@ -185,21 +191,21 @@ export default function Component() {
           technology: "HeatShrink",
           type: terminationType,
           voltage: formData.voltage,
-          core: formData.core,  // Removed .toUpperCase()
-          size: formData.size,  // Removed .replace()
+          core: formData.core, // Removed .toUpperCase()
+          size: formData.size, // Removed .replace()
           cabletype: formData.cableType,
-          conductor: formData.material,  // Removed .toUpperCase() here as well
+          conductor: formData.material, // Removed .toUpperCase() here as well
           sku: sku,
           quantity: quantity,
-          name: name
+          name: name,
         }
       );
-  
+
       if (res?.data?.id) {
         const key = "3mItems";
         updateLocalStorageArray(key, res.data.id);
       }
-  
+
       toast({
         description: "Added to Cart Successfully",
       });
@@ -209,7 +215,6 @@ export default function Component() {
       setLoading(false);
     }
   };
-  
 
   return (
     <div className="container mx-auto px-4 py-4">
@@ -222,7 +227,7 @@ export default function Component() {
             <div className="w-full lg:w-1/2 p-4 lg:max-h-[calc(100vh-2rem)] lg:overflow-y-auto">
               <div className="sticky top-0 bg-background pt-4">
                 <div className="relative h-[300px] md:h-[400px] lg:h-[500px] mb-4">
-                <Image
+                  <Image
                     src={productImages[selectedImage]}
                     alt={`Product Image ${selectedImage + 1}`}
                     layout="fill"
@@ -298,17 +303,18 @@ export default function Component() {
                     onValueChange={(value) =>
                       handleInputChange("termination", value)
                     }
-                    disabled={!formData.cableType}
                   >
                     <SelectTrigger id="termination">
                       <SelectValue placeholder="Select termination" />
                     </SelectTrigger>
                     <SelectContent>
-                      {availableTerminations.map((option) => (
-                        <SelectItem key={option} value={option}>
-                          {option}
-                        </SelectItem>
-                      ))}
+                      {["indoor", "outdoor", "straight-through"].map(
+                        (option) => (
+                          <SelectItem key={option} value={option}>
+                            {option}
+                          </SelectItem>
+                        )
+                      )}
                     </SelectContent>
                   </Select>
                 </div>
@@ -319,13 +325,12 @@ export default function Component() {
                     onValueChange={(value) =>
                       handleInputChange("voltage", value)
                     }
-                    disabled={!formData.cableType}
                   >
                     <SelectTrigger id="voltage">
                       <SelectValue placeholder="Select voltage" />
                     </SelectTrigger>
                     <SelectContent>
-                      {availableVoltages.map((option) => (
+                      {allVoltageOptions.map((option) => (
                         <SelectItem key={option} value={option}>
                           {option}
                         </SelectItem>
@@ -338,13 +343,12 @@ export default function Component() {
                   <Label htmlFor="core">Core</Label>
                   <Select
                     onValueChange={(value) => handleInputChange("core", value)}
-                    disabled={!formData.cableType || !formData.voltage}
                   >
                     <SelectTrigger id="core">
                       <SelectValue placeholder="Select core" />
                     </SelectTrigger>
                     <SelectContent>
-                      {availableCores.map((option) => (
+                      {allCoreOptions.map((option) => (
                         <SelectItem key={option} value={option}>
                           {option}
                         </SelectItem>
--- src/app/(products)/3m/heat-shrink-cable-jointing-kit/skuGenerator.ts
+++ src/app/(products)/3m/heat-shrink-cable-jointing-kit/skuGenerator.ts
@@ -1,5 +1,3 @@
-
-
 // utils/skuGenerator.ts
 import {
   Technology,
@@ -8,71 +6,113 @@ import {
   CoreType,
   SizeType,
   CableType,
-  ConductorType
-} from './skuTypes';
+  ConductorType,
+} from "./skuTypes";
 
 interface SkuMappings {
-    [key: string]: string;
-  }
+  [key: string]: string;
+}
 
-export const technology: Technology[] = ['Heat Shrink'];
+export const technology: Technology[] = ["Heat Shrink"];
 
-export const terminationTypes: TerminationType[] = ['Indoor', 'Outdoor', 'Straight through'];
-export const voltageTypes: VoltageType[] = ['1.1KV(E)', '6.6KV(UE)/11KV(E)', '11KV(UE)/22KV(E)', '22KV(E)', '33KV(E)', '33KV(UE)', '66KV(E)'];
-export const coreTypes: CoreType[] = ['1 CORE', '2 CORE', '3 CORE', '3.5 CORE', '4 CORE'];
-export const sizeTypes: SizeType[] = ['6mm', '10mm', '16mm', '25mm', '35mm', '50mm', '70mm', '95mm', '120mm', '150mm', '185mm', '240mm', '300mm', '400mm', '500mm', '630mm', '800mm', '1000mm'];
-export const cableTypes: CableType[] = ['XLPE/PVC', 'EPR', 'ABC'];
-export const conductorTypes: ConductorType[] = ['ALUMINIUM', 'COPPER'];
+export const terminationTypes: TerminationType[] = [
+  "Indoor",
+  "Outdoor",
+  "Straight through",
+];
+export const voltageTypes: VoltageType[] = [
+  "1.1KV(E)",
+  "6.6KV(UE)/11KV(E)",
+  "11KV(UE)/22KV(E)",
+  "22KV(E)",
+  "33KV(E)",
+  "33KV(UE)",
+  "66KV(E)",
+];
+export const coreTypes: CoreType[] = [
+  "1 CORE",
+  "2 CORE",
+  "3 CORE",
+  "3.5 CORE",
+  "4 CORE",
+];
+export const sizeTypes: SizeType[] = [
+  "6mm",
+  "10mm",
+  "16mm",
+  "25mm",
+  "35mm",
+  "50mm",
+  "70mm",
+  "95mm",
+  "120mm",
+  "150mm",
+  "185mm",
+  "240mm",
+  "300mm",
+  "400mm",
+  "500mm",
+  "630mm",
+  "800mm",
+  "1000mm",
+];
+export const cableTypes: CableType[] = ["XLPE", "PVC", "EPR", "ABC"];
+export const conductorTypes: ConductorType[] = ["ALUMINIUM", "COPPER"];
 
 const typeToSkuMap: SkuMappings = {
-  'Indoor': 'I',
-  'Outdoor': 'O',
-  'Straight through': 'S'
+  Indoor: "I",
+  Outdoor: "O",
+  "Straight through": "S",
 };
 
 const typeToSku = (type: TerminationType): string => typeToSkuMap[type];
 
-const cableTypeToSku = (cableType: CableType, type: TerminationType): string => {
-  if (cableType === 'XLPE/PVC') {
-    return type === 'Straight through' ? 'XXA' : 'X_A';
-  } else if (cableType === 'EPR') {
-    return 'TCU';
-  } else if (cableType === 'ABC') {
-    return 'ABU';
+const cableTypeToSku = (
+  cableType: CableType,
+  type: TerminationType
+): string => {
+  if (cableType === "XLPE" || cableType == "PVC") {
+    return type === "Straight through" ? "XXA" : "X_A";
+  } else if (cableType === "EPR") {
+    return "TCU";
+  } else if (cableType === "ABC") {
+    return "ABU";
   }
-  return '';
+  return "";
 };
 
 const conductorToSkuMap: SkuMappings = {
-  'ALUMINIUM': 'A',
-  'COPPER': 'C'
+  ALUMINIUM: "A",
+  COPPER: "C",
 };
 
-const conductorToSku = (conductor: ConductorType): string => conductorToSkuMap[conductor];
+const conductorToSku = (conductor: ConductorType): string =>
+  conductorToSkuMap[conductor];
 
 const voltageToSkuMap: SkuMappings = {
-  '1.1KV(E)': '1.1E',
-  '6.6KV(UE)/11KV(E)': '011E',
-  '11KV(UE)/22KV(E)': '022E',
-  '22KV(E)': '022E',
-  '33KV(E)': '033E',
-  '33KV(UE)': '033U',
-  '66KV(E)': '066E'
+  "1.1KV(E)": "1.1E",
+  "6.6KV(UE)/11KV(E)": "011E",
+  "11KV(UE)/22KV(E)": "022E",
+  "22KV(E)": "022E",
+  "33KV(E)": "033E",
+  "33KV(UE)": "033U",
+  "66KV(E)": "066E",
 };
 
 const voltageToSku = (voltage: VoltageType): string => voltageToSkuMap[voltage];
 
 const coreToSkuMap: SkuMappings = {
-  '1 CORE': '01C',
-  '2 CORE': '02C',
-  '3 CORE': '03C',
-  '3.5 CORE': '3.5C',
-  '4 CORE': '04C'
+  "1 CORE": "01C",
+  "2 CORE": "02C",
+  "3 CORE": "03C",
+  "3.5 CORE": "3.5C",
+  "4 CORE": "04C",
 };
 
 const coreToSku = (core: CoreType): string => coreToSkuMap[core];
 
-const sizeToSku = (size: SizeType): string => size.replace('mm', '').padStart(4, '0');
+const sizeToSku = (size: SizeType): string =>
+  size.replace("mm", "").padStart(4, "0");
 
 export const generateSku = (
   tech: Technology,
@@ -83,5 +123,10 @@ export const generateSku = (
   cableType: CableType,
   conductor: ConductorType
 ): string => {
-  return `3MH${typeToSku(type)}_${cableTypeToSku(cableType, type)}${conductorToSku(conductor)}_${voltageToSku(voltage)}_${coreToSku(core)}${sizeToSku(size)}`;
+  return `3MH${typeToSku(type)}_${cableTypeToSku(
+    cableType,
+    type
+  )}${conductorToSku(conductor)}_${voltageToSku(voltage)}_${coreToSku(
+    core
+  )}${sizeToSku(size)}`;
 };
